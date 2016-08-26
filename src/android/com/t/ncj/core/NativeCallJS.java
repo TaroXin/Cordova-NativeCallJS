a09fd6639c5bbfd8126838decde6fa0c685d3383
Taro <15029352778@163.com>
2016-08-26 22:18:28 +0800

    '创建插件'

---
+++ src/android/com/t/ncj/core/NativeCallJS.java
@@ -0,0 +1,20 @@
+package com.t.ncj.core;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by Taro on 2016/8/26.
+ */
+public class NativeCallJS extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        
+
+
+        return false;
+    }
+}
